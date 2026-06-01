c3ddacb7950e681b1267cc777290ff6c0765cce0
casaraizrd <cordovamia53@gmail.com>
2026-06-01 08:42:52 -0700

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyC0rYj7ZssPN7u6TSSA8ayXw2Ce2RlU8uQ",
+  authDomain: "crm-casa-rai.firebaseapp.com",
+  projectId: "crm-casa-rai",
+  storageBucket: "crm-casa-rai.firebasestorage.app",
+  messagingSenderId: "867390894417",
+  appId: "1:867390894417:web:75b856c9aa2fead71fe370"
+};
